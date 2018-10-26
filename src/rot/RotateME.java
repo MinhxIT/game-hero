2d00e62c69e6b915a913dbe0e11f910671b015b3
MinhxIT <varanenguyen@gmail.com>
2018-10-26 22:12:20 +0700

    Game hero

---
+++ src/rot/RotateME.java
@@ -0,0 +1,26 @@
+package rot;
+
+import javax.swing.JFrame;
+
+public class RotateME extends JFrame {
+
+    public static final int WIDTH = 800, HEIGHT = 600;
+
+    public RotateME() {
+
+        add(new Board());
+
+        setTitle("rotate me please");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new RotateME();
+
+    }
+
+}
